a7e9f2ce3887b2ce8cf1e6a75f0b6b95419c262e
stevolopolis <stevenluots@gmail.com>
2021-12-04 12:46:47 -0500

    First commit.

---
+++ cbi_deal_scrape.py
@@ -0,0 +1,178 @@
+import csv
+import math
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def get_driver():
+    options = webdriver.chrome.options.Options()
+    options.add_argument("user-data-dir=C:\\Users\\steve\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 2")
+    #options.add_argument("--disable-extensions")
+    options.add_argument("profile-directory=Profile 2")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+    return driver
+
+
+def save_data_to_csv(save_path, rows, file_exists=False):
+    print('Saving rows...')
+    if not file_exists:
+        with open(save_path, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(rows)
+    else:
+        with open(save_path, 'a', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerows(rows)
+
+# 2015 -- https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-f85da5b0-bd1c-3228-ac61-48063feff484&tab=companyDeal -- 8056
+# 2016 -- https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-47e72abd-a74d-33f8-b72e-a80d82ad9d27&tab=companyDeal -- 8398
+# 2017 -- https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-d8114bfb-f7bc-3690-8445-fcc9c54bcaa0&tab=companyDeal -- 9245
+# 2018 -- https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-6639b78a-e353-3b4c-b799-2853b5c13d52&tab=companyDeal -- 10356
+# 2019 -- https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-7694ef6d-5e05-3824-83e4-f6bb9872f655&tab=companyDeal -- 10052
+# 2020 -- https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-20663eca-9f8c-38c0-81cb-c257f65f4c52&tab=companyDeal -- 9073
+# 2021 -- https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-b06ad0b8-f322-3a34-9b2a-d15eea37b7e5&tab=companyDeal -- 11966
+
+LINKS = ['https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-f85da5b0-bd1c-3228-ac61-48063feff484&tab=companyDeal',
+         'https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-47e72abd-a74d-33f8-b72e-a80d82ad9d27&tab=companyDeal',
+         'https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-d8114bfb-f7bc-3690-8445-fcc9c54bcaa0&tab=companyDeal',
+         'https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-6639b78a-e353-3b4c-b799-2853b5c13d52&tab=companyDeal',
+         'https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-7694ef6d-5e05-3824-83e4-f6bb9872f655&tab=companyDeal',
+         'https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-20663eca-9f8c-38c0-81cb-c257f65f4c52&tab=companyDeal',
+         'https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-b06ad0b8-f322-3a34-9b2a-d15eea37b7e5&tab=companyDeal']
+LINK = 'https://app.cbinsights.com/top-search/kg7paa?companyView=table&isid=0-47e72abd-a74d-33f8-b72e-a80d82ad9d27&tab=companyDeal'
+USERNAME = 'stevents.luo@mail.utoronto.ca'
+PASSWORD = 'rma.HF!v38N5*jq'
+CSV_SAVE_PATH = 'scraped_deal_data_15.csv'
+
+year = 17
+for link in LINKS[2:]:
+    LINK = link
+    CSV_SAVE_PATH = 'scraped_deal_data_%s.csv' % year
+    year += 1
+    driver = get_driver()
+    driver.get(LINK)
+    try:
+        """Log in to CB-Insights"""
+        """email = driver.find_element(By.NAME, 'email')
+        pwd = driver.find_element(By.NAME, 'password')
+        email.send_keys(USERNAME)
+        pwd.send_keys(PASSWORD)
+
+        driver.find_element(By.XPATH, '//*[@id="hero"]/div/section/form/div[3]/button').click()"""
+
+        n_deals_html = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[2]/div/div/div[1]/div')
+        n_deals = n_deals_html.text
+        n_deals = n_deals.split(' ')[5]
+        n_pages = math.ceil(int(n_deals) / 25)
+
+        """Saving header"""
+        # headers for "deals" tab
+        header_ls = [['Companies', 'Investment Stage', 'Deal Size', 'Deal Date', 'Round Investors', 'Description', 'Industry', 'Country', 'Total Funding', 'All Investors']]
+        save_data_to_csv(CSV_SAVE_PATH, header_ls, file_exists=False)
+
+        # Currently, you could only manualyl input the number of pages
+        for page in range(1, n_pages + 1):
+            print('Scraping page %s out of %s pages' % (page, n_pages))
+
+            """Scrape table values in current page"""
+            driver.implicitly_wait(10)
+
+            companies_ls = []
+
+            for row in range(1, 26):
+                # print('Collecting data for row %s' % row)
+                try:
+                    company_ls = []
+                    for col in range(2, 12):
+                        # Columns: ['Companies', 'Investment Stage', 'Deal Size', 'Deal Date', 'Round Investors', 'Description', 'Industry', 'Country', 'Total  Funding', 'All Investors']
+                        if col in [2]:
+                            c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div/div[1]/div/div/div/div' % (row, col))
+                            company_ls.append(c_col.text)   
+                        elif col in [3, 4, 9, 10]:
+                            c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div[1]/div/div/div' % (row, col))
+                            company_ls.append(c_col.text)
+                        # Investor Column with hyperlinks to each investor
+                        elif col in [6, 11]:
+                            # Commented out due to efficiency issues.
+                            # These are investment info columns ("current-stage investors" and "all investors")
+                            """sub_row_str_ls = []
+                            more_rows = True
+                            sub_row = 1
+                            while more_rows:
+                                try:
+                                    print(sub_row_str_ls)
+                                    c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div[1]/div/div/div/div/div[%s]/div/div/div/a' % (row, col, sub_row))
+                                    sub_row_str_ls.append(c_col.text)
+                                    sub_row += 1
+                                except:
+                                    more_rows = False
+                            sub_row_str = ' '.join(sub_row_str_ls)
+                            company_ls.append(sub_row_str)"""
+                            company_ls.append('N/A')
+                        elif col in [5]:
+                            c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div[1]/div/div/p' % (row, col))
+                            company_ls.append(c_col.text)
+                        elif col in [7, 8]:
+                            c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div[1]/div/div/div/div' % (row, col))
+                            company_ls.append(c_col.text)
+                            
+                    companies_ls.append(company_ls)
+                except:
+                    try:
+                        company_ls = []
+                        c_name = driver.find_element(By.XPATH,
+                                                            '//*[@id="react-tabs-3"]/div/div/div[1]/div/div[2]/div[%s]/div/div[2]/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div/a' % row)
+                        company_ls.append(c_name.text)
+
+                        for col in range(2, 12):
+                            # Columns: ['Companies', 'Investment Stage', 'Deal Size', 'Deal Date', 'Description', 'Industry', 'Country', 'Total  Funding', 'All Investors']
+                            if col in [2]:    
+                                c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div/a' % (row, col))
+                                company_ls.append(c_col.text)   
+                            elif col in [3, 4, 9, 10]:
+                                c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div[1]/div/div/div' % (row, col))
+                                company_ls.append(c_col.text)
+                            # Investor Column with hyperlinks to each investor
+                            elif col in [6, 11]:
+                                # Commented out due to efficiency issues.
+                                # These are investment info columns ("current-stage investors" and "all investors")
+                                """sub_row_str_ls = []
+                                more_rows = True
+                                sub_row = 1
+                                while more_rows:
+                                    try:
+                                        print(sub_row_str_ls)
+                                        c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div[1]/div/div/div/div/div[%s]/div/div/div/a' % (row, col, sub_row))
+                                        sub_row_str_ls.append(c_col.text)
+                                        sub_row += 1
+                                    except:
+                                        more_rows = False
+                                sub_row_str = ' '.join(sub_row_str_ls)
+                                company_ls.append(sub_row_str)"""
+                                company_ls.append('N/A')
+                            elif col in [5]:
+                                c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div[1]/div/div/p' % (row, col))
+                                company_ls.append(c_col.text)
+                            elif col in [7, 8]:
+                                c_col = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[1]/div/div[2]/div[%s]/div/div[%s]/div/div/div[1]/div/div/div/div' % (row, col))
+                                company_ls.append(c_col.text)
+                                
+                        companies_ls.append(company_ls)
+                    except:
+                        pass
+
+            save_data_to_csv(CSV_SAVE_PATH, companies_ls, file_exists=True)
+            """Move on to the next page"""
+            driver.implicitly_wait(10)
+            next_page_button = driver.find_element(By.XPATH, '//*[@id="react-tabs-5"]/div/div/div[2]/div/div/div[3]/div/div/button[12]')
+            next_page_button.click()
+
+    finally:
+        driver.quit()
+
+#cell1 = driver.find_element_by_class_name('flexItem flexItem--shrink')
+
+#print(len(cell1))
+
---
+++ funding.py
@@ -0,0 +1,116 @@
+import csv
+import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class Deal:
+    """A funding deal made to a company.
+    
+    Rounds include: seed, A-E, IPO.
+
+    This dataclass will be used to store data from the csv files in a more orderly manner.
+
+    Instance Attributes:
+        - company: the name of the funded company
+        - stage: funding stage
+        - deal_size: size of the deal in USD
+        - deal_date: date of the funding deal
+        - r_investors: list of investors in the current funding round
+        - description: basic description of the company
+        - industry: industry of the company (e.g. technology, software)
+        - country: country of the company
+        - total_funding: total amount of funding in USD received by the company
+        - a_investors: list of all investors that has invested into the company
+    """
+    company = str
+    stage = str
+    deal_size = int
+    deal_date = datetime.datetime
+    r_investors = list[str]
+    description = str
+    industry = str
+    country = str
+    total_funding = int
+    a_investors = list[str]
+
+
+@dataclass
+class CompressedDeal: 
+    """A funding deal made to a company.
+    
+    Rounds include: seed, A-E, IPO.
+
+    This dataclass is a compressed version of Deal which could save memory space during inference.
+    It will be used exclusively for analyzing any patterns in raw funding amounts.
+
+    Instance Attributes:
+        - company: the name of the funded company
+        - stage: funding stage
+        - deal_size: size of the deal in USD
+        - deal_date: date of the funding deal
+        - industry: industry of the company (e.g. technology, software)
+        - country: country of the company
+        - total_funding: total amount of funding in USD received by the company
+    """
+    company = str
+    stage = str
+    deal_size = int
+    deal_date = datetime.datetime
+    industry = str
+    country = str
+    total_funding = int
+
+
+def read_csv(csv_path: str) -> list[CompressedDeal]:
+    """"""
+    deal_ls = []
+    with open(csv_path) as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)  # skip the header
+
+        for deal_row in reader:
+            deal = CompressedDeal(deal_row[0],
+                                  deal_row[1],
+                                  str_funding_to_int(deal_row[2]),
+                                  str_deal_date_to_datetime(deal_row[3]),
+                                  deal_row[6],
+                                  deal_row[7],
+                                  str_funding_to_int(deal_row[8]))
+
+            deal_ls.append(deal)
+
+    return deal_ls
+
+
+def str_funding_to_int(funding_str: str) -> int:
+    """Convert funding strings into integer values.
+    
+    Funding strings in CSV files are in the following format: '$1,000.00M'
+    
+    Sample Usage:
+
+    >>> str_funding_to_int('$1,000.00M')
+    1000000
+    >>> str_funding_to_int('$0.02M')
+    20000
+    """
+    funding_str = funding_str[1:-1].replace(',', '') # remove '$', 'M', and ',' from string
+    funding_int = int(funding_str) * 1000000 
+    return funding_int
+
+
+def str_deal_date_to_datetime(deal_date_str: str) -> datetime.datetime:
+    """Convert deal date strings into datetime objects.
+    
+    Deal date strings in CSV files are in the following format: '10/19/2017'
+    
+    Sample Usage:
+    
+    >>> str_deal_date_to_datetime('10/19/2017')
+    datetime.datetime(2017, 19, 10)
+    """
+    day = deal_date_str.split('/')[0]
+    month = deal_date_str.split('/')[1]
+    year = deal_date_str.split('/')[2]
+    return datetime.datetime(year, month, day)
