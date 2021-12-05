276d9019bb9a0857e2aed6dc1626a3a2aadf60c6
stevolopolis <stevenluots@gmail.com>
2021-12-05 04:08:32 -0500

    Successfully added code for raw funding line graph across multiple years.

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from data_loader import read_csv
 from data_analysis import funding_per_month, funding_per_category
 from visualization import vis_funding_per_month, vis_category_line
 
-"""sum_monthly_deals = []
-for year in range(15, 22):
+sum_monthly_deals = {}
+for year in range(15, 21):
     csv_filepath = 'scraped_deal_data_%s.csv' % year
     deals = read_csv(csv_filepath, compressed=True)
     monthly_deals = funding_per_month(deals, metric='sum')
-    sum_monthly_deals.extend(monthly_deals)
+    for month in monthly_deals:
+        date = '%s-%s' % (year, month)
+        sum_monthly_deals[date] = monthly_deals[month]
 
-vis_funding_per_month(sum_monthly_deals, '2015-2021 Total Funding Graph')"""
+vis_funding_per_month(sum_monthly_deals, '2015-2020 Total Funding Graph')
 
 
-# For categorized Visualization
+"""# For categorized Visualization
 csv_filepath = 'scraped_deal_data_15.csv'
 deals = read_csv(csv_filepath, compressed=True)
 categorized_deals = funding_per_category(deals, category='stage', metric='sum')
-vis_category_line(categorized_deals, 'stage', 'Category: stage')
+vis_category_line(categorized_deals, 'stage', 'Category: stage')"""
