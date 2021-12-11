20e35417c64075bd161cbb223515de8a1183e001
stevolopolis <stevenluots@gmail.com>
2021-12-11 18:41:08 -0500

    Added functions for visualizing pies for fundings during covid period.

--- visualization.py
+++ visualization.py
@@ -3,6 +3,9 @@ import math
 from typing import Optional
 
 
+COLORS = ['gold', 'lightcoral', 'yellowgreen', 'turquoise', 'cornflowerblue', 'mediumpurple', 'orange', 'palegreen', 'slategray', 'peru']
+
+
 def vis_funding_per_interval(interval_deals: dict[str, int], title: str, label: str, ax: Optional[plt.Axes] = None) -> None:
     """Visualizes the deals with given intervals using a line graph."""
 
@@ -59,8 +62,22 @@ def vis_multi_category_line(funding_per_cat: dict[str, dict[str, int]], title: s
     plt.show()
 
 
-def vis_category_pie(funding_per_cat: dict[str, dict[str, int]], category: str) -> None:
-    """Visualize multiply monthly deals based on selected category using a pie chart."""
+def vis_category_pie(funding_per_cat: dict[str, int], title: str, top_n: Optional[int] = 4, ax: Optional[plt.Axes] = None) -> None:
+    """Visualize top funding amounts during COVID period based on selected category using a pie chart."""
+    ranked_categories = sort_dict_to_list(funding_per_cat)
+    top_n_ranked_categories = choose_top_n(ranked_categories, top_n)
+    labels = list(top_n_ranked_categories.keys())
+    sizes = list(top_n_ranked_categories.values())
+    sizes_millions = [round(num / 1000000, 2) for num in sizes]
+
+    custom_colors = COLORS[:top_n+1]
+
+    if ax:
+        ax.pie(sizes_millions, labels=labels, colors=custom_colors, autopct='%.1fM', startangle=90)
+        ax.set_title(title)
+    else:
+        plt.pie(sizes_millions, labels=labels, colors=custom_colors, autopct='%.1fM', startangle=90)
+        plt.title(title)
 
 
 def vis_covid_line(interval):
@@ -68,3 +85,22 @@ def vis_covid_line(interval):
         plt.plot('20-Jan', color='r', linestyle='-', label='Start of COVID')
     elif interval == 'quarter':
         plt.axvline(x='20-Q1', color='r', linestyle='-', label='Start of COVID')
+
+
+def sort_dict_to_list(input: dict[str, int]) -> dict[str, int]:
+    sorted_list = sorted([(value, key) for key, value in input.items()], reverse=True)
+    return {key: value for value, key in sorted_list}
+
+
+def choose_top_n(input: dict[str, int], top_n: int) -> list[tuple[str, int]]:
+    top_n_dict = {}
+    others_sum = 0
+    for i, key in enumerate(input):
+        if i >= top_n:
+            others_sum += input[key]
+        else:
+            top_n_dict[key] = input[key]
+    top_n_dict['others'] = others_sum
+
+    return top_n_dict
+
